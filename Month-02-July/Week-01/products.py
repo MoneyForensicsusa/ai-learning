1bf97133da6a659b4d759d7c3d87e342d1262e4d
Wonderful Eyeh <wonderfuleyeh25@gmail.com>
2026-06-08 11:39:44 -0500

    rewrote the entire functions in the products file in order for it to auto commit and auto close the connection when done

--- Month-02-July/Week-01/products.py
+++ Month-02-July/Week-01/products.py
@@ -2,22 +2,18 @@ import sqlite3
 
 # function that adds product information using parameterized query
 def add_product(name, price, category):
-    conn = sqlite3.connect('july_learning.db')
-    cursor = conn.cursor()
+    with sqlite3.connect("july_learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO products (name, price, category) VALUES (?, ?, ?)',(name, price, category))
 
-    cursor.execute('INSERT INTO products (name, price, category) VALUES (?, ?, ?)',(name, price, category))
-    conn.commit()
-    conn.close()
 add_product('orange_juice', 50, 'fruits')
 
 # function that reyurns all products in a particular category as a list of tuples
 def get_by_category(category):
-    conn = sqlite3.connect("july_learning.db")
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM products WHERE category = ?', (category,))
-    rows = cursor.fetchall()
-    conn.close()
+    with sqlite3.connect("july_learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM products WHERE category = ?', (category,))
+        rows = cursor.fetchall()
     return rows
 
 products = get_by_category('fruits')
@@ -27,12 +23,10 @@ for product in products:
 
 # function that returns all products above a given price, and sorted by desc price
 def get_expensive(min_price):
-    conn = sqlite3.connect("july_learning.db")
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM products WHERE price > ? ORDER BY price DESC', (min_price,))
-    rows = cursor.fetchall()
-    conn.close()
+    with sqlite3.connect("july_learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM products WHERE price > ? ORDER BY price DESC', (min_price,))
+        rows = cursor.fetchall()
     return rows
 products = get_expensive(51)
 print(products)
