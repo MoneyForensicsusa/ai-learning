4c04b2b7071cef6c9ac2df34e1f1c5219dd89c21
Wonderful Eyeh <wonderfuleyeh25@gmail.com>
2026-06-11 18:00:42 -0500

    rewrote the functions in customer_order.py to use python context manager pattern

--- Month-02-July/Week-02/customer_order.py
+++ Month-02-July/Week-02/customer_order.py
@@ -16,20 +16,42 @@ def get_connection():
 
 
 def get_customer_orders(customer_id):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
-        SELECT customers.name, products.name, orders.quantity
-        FROM orders
-        INNER JOIN customers ON orders.customer_id = customers.id
-        INNER JOIN products ON orders.product_id = products.id
-        WHERE orders.customer_id = %s''',
-        (customer_id,)
-    )
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                    SELECT customers.name, products.name, orders.quantity
+                    FROM orders
+                    INNER JOIN customers ON orders.customer_id = customers.id
+                    INNER JOIN products ON orders.product_id = products.id
+                    WHERE orders.customer_id = %s''',
+                    (customer_id,)
+                )
+                rows = cursor.fetchall()
+        return rows
+    except psycopg2.OperationalError:
+        print(f"Cannot connect to database: {e}")
+        return[]
+
+    
 orders = get_customer_orders(2)
 for order in orders:
     print(f'Customer: {order[0]}, Product: {order[1]}, Quantity: {order[2]}')
 
+def add_order(customer_id, product_id, quantity):
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                    INSERT INTO orders (customer_id, product_id, quantity)
+                    VALUES (%s, %s, %s)
+                    RETURNING id''',
+                    (customer_id, product_id, quantity))
+                new_id = cursor.fetchone()[0]
+                conn.commit()
+        return new_id
+    except psycopg2.OperationalError:
+        print(f"Cannot connect to database: {e}")
+        return None
+new_id = add_order(2, 1, 5)
+print(new_id)
